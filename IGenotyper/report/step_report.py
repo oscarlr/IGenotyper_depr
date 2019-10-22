f8895ba610dffaf591da18031986a84562035dd7
Oscar L. Rodriguez <oscar.rodriguez@icahn.mssm.edu>
2019-10-22 15:57:17 -0400

    Create step_report.py

---
+++ IGenotyper/report/step_report.py
@@ -0,0 +1,38 @@
+#!/bin/env python
+
+def get_coverage():
+ 	pass
+  
+def get_region_coverage(region):
+ 	pass
+
+def get_region_SNV_count(region):
+	pass
+	
+def get_assembly_size():
+	pass
+	
+def get_number_of_contigs():
+	pass
+	
+def get_RSS_SNV_count():
+	pass
+	
+def get_leader_part_SNV_count():
+	pass
+ 
+def get_intron_SNV_count():
+	pass
+
+def get_deletion_count():
+	pass
+	
+def get_insertion_count():
+	pass
+	
+def get_alleles(gene_type):
+	pass
+	
+def get_novel_alleles():
+	pass
+	
