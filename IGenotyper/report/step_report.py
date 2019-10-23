891f26b7ad9ae034e1cb48c8edb137e1782afc88
Oscar L. Rodriguez <oscar.rodriguez@icahn.mssm.edu>
2019-10-23 13:28:32 -0400

    Update step_report.py

--- IGenotyper/report/step_report.py
+++ IGenotyper/report/step_report.py
@@ -6,8 +6,21 @@ def get_coverage():
 def get_region_coverage(region):
  	pass
 
-def get_region_SNV_count(region):
-	pass
+def get_SNV_count(vcf_file,info_field,value,read_support=True):
+	count = 0
+	string_to_search = "%s=%s" % (info_field,value)
+	with open(vcf_file,'r') as fh:
+		for line in fh:
+			line = line.rstrip().split('\t')
+			if line[0] == "#":
+				continue
+			if string_to_search in line[XXX]:
+				if read_support:
+					if "read_support=Yes" in line[XXX]:
+						count += 1
+				else:
+					count += 1
+	return count
 	
 def get_assembly_size():
 	pass
@@ -40,6 +53,7 @@ def get_SV_genotypes():
 	pass
 
 def write_report(self):
+	snv_total_count = get_SNV_count(self.phased_variants_vcf,"read_support","Yes")
 	report = [
 		["IGH Coverage (CCS)",get_coverage("ccs")],
 		["IGH Coverage (subreads)",get_coverage("subreads")],
@@ -48,12 +62,12 @@ def write_report(self):
 		["IGHV Coverage",get_region_coverage("IGHV")],
 		["IGH assembly size (bp)",get_assembly_size()],
 		["IGH assembly number of contigs",get_number_of_contigs()],
-		["# of SNVs in IGHJ region",get_SNV_count("IGHJ")],
-		["# of SNVs in IGHD region",get_SNV_count("IGHD")],
-		["# of SNVs in IGHV region",get_SNV_count("IGHV")],
-		["# of SNVs in RSS",get_SNV_count("IGHV")],
-		["# of SNVs in LP1",get_SNV_count("IGHV")],
-		["# of SNVs in introns",get_SNV_count("IGHV")],
+		["# of SNVs in IGHJ region",get_SNV_count(self.phased_variants_vcf,"region","IGHJ")],
+		["# of SNVs in IGHD region",get_SNV_count(self.phased_variants_vcf,"region","IGHD")],
+		["# of SNVs in IGHV region",get_SNV_count(self.phased_variants_vcf,"region","IGHV")],
+		["# of SNVs in RSS",snv_total_count - get_SNV_count(self.phased_variants_vcf,"RSS","None")],
+		["# of SNVs in LP1",snv_total_count - get_SNV_count(self.phased_variants_vcf,"LP1","None")],
+		["# of SNVs in introns",snv_total_count - get_SNV_count(self.phased_variants_vcf,"intronic","None")],
 		["# of deletions (>3bps)",get_deletion_count()],
 		["# of insertions (>3bps)",get_insertion_count()],
 		["IGHJ alleles",get_alleles("IGHJ")],
