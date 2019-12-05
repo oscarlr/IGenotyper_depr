2b50b3b119910e9d9517478fee2b5c0515db8f5c
Oscar <olrodriguez01@gmail.com>
2019-12-05 12:04:40 -0500

    adding change for 3-30

--- IGenotyper/phase/step_phase_reads.py
+++ IGenotyper/phase/step_phase_reads.py
@@ -9,6 +9,102 @@ from Bio.SeqRecord import SeqRecord
 from ..command_line import *
 from ..common import *
 
+def load_het_snvs(snvs):
+    positions = []
+    with open(snvs,'r') as fh:
+        for line in fh:
+            line = line.rstrip().split('\t')
+            if "0/1" in line or "0|1" in line or "1|0" in line:
+                positions.append(int(line[1]))
+    return positions
+
+def read_is_unphased(read):
+    haplotype = read.get_tag("RG",True)[0]
+    if haplotype == "0":
+        return True
+    return False
+
+def read_overlap_hets(read,het_snvs):
+    het_snvs = sorted(het_snvs)
+    for snv in het_snvs:
+        if snv < read.reference_start:
+            continue
+        if snv > read.reference_end:
+            return False
+        return True
+    return False
+
+def get_secondary_alignments(phased_ccs_reads):
+    secondary_alignments = {}
+    samfile = pysam.AlignmentFile(phased_ccs_reads)
+    for read in samfile:
+        if read.is_secondary:
+            if read.query_name not in secondary_alignments:
+                secondary_alignments[read.query_name] = []
+            secondary_alignments[read.query_name].append(read)
+    return secondary_alignments
+
+def change_read(primary_read,secondary_reads):
+    secondary_to_primary_read = None
+    secondary_to_primary_flag = None
+    secondary_to_primary_mapq = None
+    index_to_ignore = None
+    secondary_to_primary_score = -9999999999
+    for i,secondary_read in enumerate(secondary_reads):
+        secondary_read_score = secondary_read.get_tag("AS",True)[0]
+        if secondary_read_score > secondary_to_primary_score:
+            secondary_to_primary_read = secondary_read
+            secondary_to_primary_score = secondary_read_score
+            secondary_to_primary_flag = secondary_read.flag
+            secondary_to_primary_mapq = secondary_read.mapping_quality
+            index_to_ignore = i
+    secondary_to_primary_read.flag = primary_read.flag
+    secondary_to_primary_read.mapping_quality = primary_read.mapping_quality
+    primary_read.flag = secondary_to_primary_flag
+    primary_read.mapping_quality = secondary_to_primary_mapq
+    reads_to_return = [primary_read,secondary_to_primary_read]
+    for i,read in enumerate(secondary_reads):
+        if i == index_to_ignore:
+            continue
+        reads_to_return.append(read)
+    return reads_to_return
+
+def change_primary_alignments(phased_ccs_reads,output_bamfile):
+    samfile = pysam.AlignmentFile(phased_ccs_reads)
+    changed_reads = set()
+    for read in samfile:
+        if read.is_secondary:
+            continue
+        if read.is_supplementary:
+            continue
+        if read_is_unphased(read) and read_overlap_hets(read,het_snvs):
+            if read.query_name in secondary_alignments:
+                secondary_reads = secondary_alignments[read.query_name]
+                output_reads = change_read(read,secondary_reads)
+                for output_read in output_reads:
+                    changed_reads.add(output_read.query_name)
+                    output_bamfile.write(output_read)
+    samfile.close()
+    return changed_reads
+
+def fix_alignments(tmp_dir,phased_ccs_reads,snvs):
+    # 1. Find the unphased reads that overlap heterozygous SNPs
+    # 2. Check if the secondary alignment of the unphased read is phased
+    # 3. Change the secondary alignment to the primary alignment
+    # 4. If the scondary alignment is still not phased, move it to the primary alignment
+    het_snvs = load_het_snvs(snvs)
+    secondary_alignments = get_secondary_alignments(phased_ccs_reads)
+    changed_bamfile = "%s/changed_alignments.bam" % tmp_dir
+    output_bamfile = pysam.AlignmentFile(changed_bamfile,"w",template=samfile)
+    changed_reads = change_primary_alignments(phased_ccs_reads,output_bamfile)
+    samfile = pysam.AlignmentFile(phased_ccs_reads)
+    for read in samfile:
+        if read.query_name in changed_reads:
+            continue
+        output_bamfile.write(read)
+    samfile.close()        
+    return changed_bamfile
+
 def phase_mapped_reads(self):
     command_line_tools = CommandLine(self)
     command_line_tools.get_ccs_reads()
@@ -18,6 +114,8 @@ def phase_mapped_reads(self):
     command_line_tools.phase_snps()
     command_line_tools.phase_ccs_reads()
     command_line_tools.phase_subreads()
+    changed_bamfile = fix_alignments(self.tmp_dir,self.phased_ccs_mapped_reads)    
+    command_line_tools.sam_to_sorted_bam(changed_bamfile[:-4],"%s.sorted.bam" % changed_bamfile[:-4])
 
     # Fix 3_30_alignments
 
