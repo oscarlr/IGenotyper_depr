97da886c8e4bbc2ad51d21ed057782ff7e6d4bcb
Oscar Rodriguez <oscarrodriguez@oscars-mbp.wireless.mssm.edu>
2019-12-05 21:24:03 -0500

    add

--- IGenotyper/phase/step_phase_reads.py
+++ IGenotyper/phase/step_phase_reads.py
@@ -147,23 +147,4 @@ def phase_mapped_reads(self):
         os.mkdir("%s/variants/from_reads" % self.outdir)
         command_line_tools.phase_snps()
         command_line_tools.phase_ccs_reads()
-
-    # Fix 3_30_alignments
-
-    # # Map subreads ands ccs reads
-    # map_reads(self.threads,self.pbmm2_ref,ccs_reads,
-    #           self.ccs_mapped_reads,"ccs")
-    # map_reads(self.threads,self.pbmm2_ref,self.input_bam,
-    #           self.subreads_mapped_reads,"subreads")  
-    
-    # # Phase snps, and mapped subreads and ccs reads
-    # phase_snps(self.pbmm2_ref,self.snp_candidates,self.variants_vcf,
-    #            self.phased_variants_vcf,self.ccs_mapped_reads,
-    #            self.snp_candidates_filtered,self.regions_to_ignore)
-    # phase_reads(self.phased_variants_vcf,self.ccs_mapped_reads,
-    #             self.phased_ccs_mapped_reads,self.phased_vcf_file_sample_name)
-    # phase_reads(self.phased_variants_vcf,self.subreads_mapped_reads,
-    #             self.phased_subreads_mapped_reads,self.phased_vcf_file_sample_name)
-    
-    # # Get haplotype blocks
-    # get_phased_blocks(self.phased_variants_vcf,self.haplotype_blocks)
+    command_line_tools.get_phased_blocks()
