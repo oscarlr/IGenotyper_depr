a85190c937c279f668f047c141477583f9a7b6f8
Oscar <olrodriguez01@gmail.com>
2020-04-09 22:25:57 -0400

    add

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='IGenotyper_clean',
+    name='IGenotyper',
     description='',
     packages=find_packages(),
     package_data={'IGenotyper': ['data/*','bash_scripts/*','python_scripts/*','r_scripts/*']},
     include_package_data=True,
     entry_points = {
-        'console_scripts': ['IG-clean = IGenotyper.main:main',
+        'console_scripts': ['IG = IGenotyper.main:main',
                             'IG-make-ref = IGenotyper.make_ref:main',
                             'IG-phase-reads =  IGenotyper.python_scripts.phase_reads:main',
                             'IG-filter-vcf =  IGenotyper.python_scripts.filter_vcf:main']
