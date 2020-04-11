15618dd3e667fc7cd3c92393bd3f2d23f39d7c5c
Oscar <olrodriguez01@gmail.com>
2020-04-11 15:30:53 -0400

    add

--- IGenotyper/main.py
+++ IGenotyper/main.py
@@ -38,12 +38,12 @@ class Sample(object):
         self.threads = None        
         self.cluster = None
         self.cluster_queue = None
-        self.cluster_threads = None
+        #self.cluster_threads = None
         self.cluster_walltime = None
         self.cluster_mem = None
 
         # Other
-        self.haploid = None
+        #self.haploid = None
         self.sv_regions = None
         self.non_sv_regions = None
         self.introns = None
@@ -78,7 +78,7 @@ class Sample(object):
         self.locus_fasta = None
         self.locus_fasta_unquivered = None
         self.locus_fastq = None
-        self.add_unphased_reads = None
+        #self.add_unphased_reads = None
         self.assembly_script = None
         self.phased_regions_with_coverage = None
         ## Merging
@@ -131,16 +131,17 @@ class Sample(object):
         ("threads","threads"),        
         ("cluster","cluster"),
         ("cluster_queue","cluster_queue"),
-        ("cluster_threads","cluster_threads"),
         ("cluster_walltime","cluster_walltime"),
         ("cluster_mem","cluster_mem"),
-        ("haploid","haploid"),
         ("phased_vcf_file","phased_variants_vcf"),
-        ("phased_vcf_file_sample_name","phased_vcf_file_sample_name"),
-        ("add_unphased_reads","add_unphased_reads"),
+        ("phased_vcf_file_sample_name","phased_vcf_file_sample_name"),        
         ("dont_split","dont_split"),
         ("secondary_read_score","secondary_read_score")
         ]
+    
+    #("cluster_threads","cluster_threads"),
+    #("haploid","haploid"),
+    #("add_unphased_reads","add_unphased_reads"),
     
     def load_attrs(self):
         file_names = FileNames(self.outdir)
@@ -163,10 +164,10 @@ class Sample(object):
         create_directory(self.outdir)
         if self.phase:
             self.phase_mapped_reads()
-        elif self.assemble:
-            self.assemble_reads()
         elif self.extend_assembly:            
             self.get_possible_merges()
+        elif self.assemble:
+            self.assemble_reads()
         elif self.detect:
             self.detect_variants()
         elif self.stats:
@@ -183,48 +184,48 @@ class Sample(object):
 def main():
     parser = argparse.ArgumentParser(description='Process IGH capture data')
     parser.add_argument('input_bam', metavar='input_bam', type=check_file_exist,
-                        help='bam file containing raw reads')
+                        help='pacbio subreads bam file')
     parser.add_argument('outdir', metavar='outdir', 
                         help='output directory')
     parser.add_argument('--phase', action='store_true', default=False,
                         help='Map and phase reads')
     parser.add_argument('--assemble', action='store_true', default=False,
-                        help='Only assemble reads')
+                        help='Assemble reads')
     parser.add_argument('--extend_assembly', action='store_true', default=False,
-                        help='Extend assemblies')
+                        help='Extend assemblies. Still testing. Dont use.')
     parser.add_argument('--detect', action='store_true', default=False,
                         help='Detect variants')
     parser.add_argument('--stats', action='store_true', default=False,
                         help='Generate stats')
     parser.add_argument('--report', action='store_true', default=False,
                         help='Generate report')
-    parser.add_argument('--tmp_dir', metavar='tmp_dir',
+    parser.add_argument('--tmp_dir', metavar='',
                         help='temporary directory')
-    parser.add_argument('--threads', metavar='threads', default=1,
+    parser.add_argument('--threads', metavar='', default=1,
                         help='Number of threads for everything')
     parser.add_argument('--cluster', action='store_true', default=False,
                         help='Use cluster')
     parser.add_argument('--cluster_queue', metavar="", default="express",
                         help='Queue for cluster')
-    parser.add_argument('--cluster_threads', metavar='', default=4,
-                        help='Number of threads for cluster jobs')
+    # parser.add_argument('--cluster_threads', metavar='', default=4,
+    #                     help='Number of threads for cluster jobs')
     parser.add_argument('--cluster_walltime', metavar="", default=2,
                         help='Walltime for cluster')
     parser.add_argument('--cluster_mem', metavar="", default=5,
                         help='memory for cluster')
-    parser.add_argument('--haploid',action='store_true', default=False,
-                        help='Run Quiver in haploid mode')
-    parser.add_argument('--phased_vcf_file', type=check_file_exist,
+    # parser.add_argument('--haploid',action='store_true', default=False,
+    #                     help='Run Quiver in haploid mode')
+    parser.add_argument('--phased_vcf_file', metavar="", type=check_file_exist,
                         help='Run IG with this phased VCF file')
-    parser.add_argument('--pacbio_data_type', default="RS",
+    parser.add_argument('--pacbio_data_type', metavar="", default="RS",
                         help='Pacbio data type (RS or Sequel), either "RS" or "SQ"')
-    parser.add_argument('--phased_vcf_file_sample_name',default="sample",
+    parser.add_argument('--phased_vcf_file_sample_name',metavar="",default="sample",
                         help='Sample name in phased VCF file')
-    parser.add_argument('--add_unphased_reads',action='store_true', default=False,
-                        help='Add unphased reads to phased region')
+    # parser.add_argument('--add_unphased_reads',action='store_true', default=False,
+    #                     help='Add unphased reads to phased region')
     parser.add_argument('--dont_split',action='store_true', default=False,
                         help='Do not split assembly regions into SV/non-SV regions')
-    parser.add_argument('--secondary_read_score',default=500,type=int,
+    parser.add_argument('--secondary_read_score',metavar="",default=500,type=int,
                         help='Min secondary read score to move')
     args = parser.parse_args()
     sample = Sample.load_args(args)
