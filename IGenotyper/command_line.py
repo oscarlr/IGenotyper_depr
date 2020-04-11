79918d70a1ff45cbcff9bfd51aef0aff12b5b93d
Oscar <olrodriguez01@gmail.com>
2020-04-11 17:39:08 -0400

    add

--- IGenotyper/command_line.py
+++ IGenotyper/command_line.py
@@ -187,99 +187,99 @@ class CommandLine():
         if not non_emptyfile(output_file):
             os.system(command)
 
-def get_bedgraph(mapped_reads,bedgraph,bam_filter):
-    args = [mapped_reads,bam_filter,bedgraph]
-    command = ("samtools view -F 3844 -Sbh %s %s | "
-               "bedtools genomecov -bg -ibam stdin > %s" % tuple(args))
-    if not non_emptyfile(bedgraph):
-        os.system(command)
-
-# def get_ccs_reads(input_bam,dir_,threads):
-#     print "Generating CCS reads..."
-#     command = ("ccs "
-#                "--numThreads %s "
-#                "--minPasses 2 "               
-#                "%s "
-#                "%s/ccs.bam > /dev/null" % (threads,input_bam,dir_))
-#     if not non_emptyfile("%s/ccs.bam.pbi" % dir_):
+# def get_bedgraph(mapped_reads,bedgraph,bam_filter):
+#     args = [mapped_reads,bam_filter,bedgraph]
+#     command = ("samtools view -F 3844 -Sbh %s %s | "
+#                "bedtools genomecov -bg -ibam stdin > %s" % tuple(args))
+#     if not non_emptyfile(bedgraph):
 #         os.system(command)
 
-def turn_reads_to_fastq(dir_,reads_in_bam):
-    print "Turnning reads to fastq..."
-    args = [dir_,dir_,dir_,dir_,reads_in_bam,dir_]
-    command = ("bam2fastq "
-               "-o %s/ccs.reads.tmp %s/ccs.bam\n"
-               "gunzip %s/ccs.reads.tmp.fastq.gz\n" 
-               "bam2fastq "
-               "-o %s/subreads.reads %s\n"
-               "gunzip %s/subreads.reads.fastq.gz" % tuple(args))
-    subreads_file = "%s/subreads.reads.fastq" % dir_ # Change back to subreads
-    if not non_emptyfile(subreads_file):
-        os.system(command)
+# # def get_ccs_reads(input_bam,dir_,threads):
+# #     print "Generating CCS reads..."
+# #     command = ("ccs "
+# #                "--numThreads %s "
+# #                "--minPasses 2 "               
+# #                "%s "
+# #                "%s/ccs.bam > /dev/null" % (threads,input_bam,dir_))
+# #     if not non_emptyfile("%s/ccs.bam.pbi" % dir_):
+# #         os.system(command)
 
-def map_reads(threads,ref,reads,sorted_bamfile,type_of_read):
-    print "Mapping %s..." % reads
-    opts=""
-    if type_of_read == "ccs":
-        opts="--preset CCS"        
-    args = [ref,reads,sorted_bamfile,threads,threads,opts]
-    command = ("pbmm2 align "
-               "%s "
-               "%s "
-               "%s "
-               "--sort "
-               "-j %s -J %s "
-               "%s " % tuple(args))
-    if not non_emptyfile("%s.bai" % sorted_bamfile):
-        print command
-        os.system(command)
+# def turn_reads_to_fastq(dir_,reads_in_bam):
+#     print "Turnning reads to fastq..."
+#     args = [dir_,dir_,dir_,dir_,reads_in_bam,dir_]
+#     command = ("bam2fastq "
+#                "-o %s/ccs.reads.tmp %s/ccs.bam\n"
+#                "gunzip %s/ccs.reads.tmp.fastq.gz\n" 
+#                "bam2fastq "
+#                "-o %s/subreads.reads %s\n"
+#                "gunzip %s/subreads.reads.fastq.gz" % tuple(args))
+#     subreads_file = "%s/subreads.reads.fastq" % dir_ # Change back to subreads
+#     if not non_emptyfile(subreads_file):
+#         os.system(command)
 
-def find_snp_candidates(ref,mapped_reads,snp_candidates):
-    args = [ref,mapped_reads,snp_candidates]
-    command = ("source activate whatshap-tool \n"
-               "whatshap find_snv_candidates "
-               "%s "
-               "%s "
-               "--pacbio "
-               "-o %s \n" % tuple(args))
-    if not non_emptyfile(snp_candidates):
-        os.system(command)
+# def map_reads(threads,ref,reads,sorted_bamfile,type_of_read):
+#     print "Mapping %s..." % reads
+#     opts=""
+#     if type_of_read == "ccs":
+#         opts="--preset CCS"        
+#     args = [ref,reads,sorted_bamfile,threads,threads,opts]
+#     command = ("pbmm2 align "
+#                "%s "
+#                "%s "
+#                "%s "
+#                "--sort "
+#                "-j %s -J %s "
+#                "%s " % tuple(args))
+#     if not non_emptyfile("%s.bai" % sorted_bamfile):
+#         print command
+#         os.system(command)
 
-def phase_snps(ref,snp_candidates,vcf,phased_vcf,ccs_mapped_reads,snp_candidates_filtered,regions_to_ignore):
-    print "Calling and phasing SNPs..."
-    args = [ref,ccs_mapped_reads,snp_candidates,
-            snp_candidates,snp_candidates_filtered,regions_to_ignore,
-            ref,vcf,snp_candidates_filtered,ccs_mapped_reads,
-            ref,phased_vcf,vcf,ccs_mapped_reads]
-    command = ("source activate whatshap-tool \n"
-               "whatshap find_snv_candidates "
-               "%s "
-               "%s "
-               "--pacbio "
-               "-o %s \n"
-               "conda deactivate \n"
-               "IG-filter-vcf %s %s %s\n "
-               "source activate whatshap-tool \n"
-               "whatshap genotype "
-               "--chromosome igh "
-               "--sample sample "
-               "--ignore-read-groups "
-               "--reference %s "
-               "-o %s "
-               "%s "
-               "%s \n"
-               "whatshap phase "
-               "--sample sample "
-               "--reference %s "
-               "--ignore-read-groups "
-               "--distrust-genotypes "
-               "-o %s "
-               "%s "
-               "%s \n"
-               "conda deactivate" % tuple(args))
-    if not non_emptyfile(phased_vcf):
-        print command
-        os.system(command)    
+# def find_snp_candidates(ref,mapped_reads,snp_candidates):
+#     args = [ref,mapped_reads,snp_candidates]
+#     command = ("source activate whatshap-tool \n"
+#                "whatshap find_snv_candidates "
+#                "%s "
+#                "%s "
+#                "--pacbio "
+#                "-o %s \n" % tuple(args))
+#     if not non_emptyfile(snp_candidates):
+#         os.system(command)
+
+# def phase_snps(ref,snp_candidates,vcf,phased_vcf,ccs_mapped_reads,snp_candidates_filtered,regions_to_ignore):
+#     print "Calling and phasing SNPs..."
+#     args = [ref,ccs_mapped_reads,snp_candidates,
+#             snp_candidates,snp_candidates_filtered,regions_to_ignore,
+#             ref,vcf,snp_candidates_filtered,ccs_mapped_reads,
+#             ref,phased_vcf,vcf,ccs_mapped_reads]
+#     command = ("source activate whatshap-tool \n"
+#                "whatshap find_snv_candidates "
+#                "%s "
+#                "%s "
+#                "--pacbio "
+#                "-o %s \n"
+#                "conda deactivate \n"
+#                "IG-filter-vcf %s %s %s\n "
+#                "source activate whatshap-tool \n"
+#                "whatshap genotype "
+#                "--chromosome igh "
+#                "--sample sample "
+#                "--ignore-read-groups "
+#                "--reference %s "
+#                "-o %s "
+#                "%s "
+#                "%s \n"
+#                "whatshap phase "
+#                "--sample sample "
+#                "--reference %s "
+#                "--ignore-read-groups "
+#                "--distrust-genotypes "
+#                "-o %s "
+#                "%s "
+#                "%s \n"
+#                "conda deactivate" % tuple(args))
+#     if not non_emptyfile(phased_vcf):
+#         print command
+#         os.system(command)    
 
 # def get_phased_blocks(phased_vcf,hap_blocks):
 #     args = [hap_blocks,phased_vcf]
